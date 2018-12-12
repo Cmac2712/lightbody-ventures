e352646afb87c2491b13e975e1c0bb4f4914fc17
Craig <craig.macintyre@indez.com>
2018-12-12 05:42:29 +0000

    Added Mumsnet page

---
+++ wp-content/themes/lightbody-ventures/dist/mumsnet-template.php
@@ -0,0 +1,41 @@
+<?php
+
+/* Template Name: Mumsnet Template */
+
+?>
+
+<?php  get_header(); ?>
+
+
+<?php get_template_part('template-parts/banner'); ?>
+
+		<?php
+		if ( have_posts() ) :
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post(); ?>
+
+				<section class="mumsnet">
+					<div class="wrapper">
+						<div class="mumsnet__logo">
+							<img src="<?php echo get_field('mumsnet_logo')['url']; ?>" alt="">
+						</div>
+
+						<h1 class="mumsnet__title"><?php echo get_field('mumsnet_title'); ?></h1>
+						<p class="mumsnet__description"><?php echo get_field('mumsnet_description'); ?></p>
+					</div>
+				</section>
+
+		<?php endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+<?php get_footer();
